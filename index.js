8335423aec6670bdee8d95887e8fe81a4e7b9e6e
Richard Morgan <richard@richardmorgan.com>
2015-06-12 18:45:39 -0500

    Generate a new testsuite for each describe block like protractor does

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// TODO:
+// should probably get rid of the root browser element, but we need somewhere to store any system-out output
+// need to add errors, tests, failures, and time to each testsuite
+
 var os = require('os');
 var path = require('path');
 var fs = require('fs');
@@ -8,8 +12,9 @@ var JUnitReporter = function(baseReporterDecorator, config, logger, helper, form
   var log = logger.create('reporter.junitp');
   var reporterConfig = config.junitpReporter || {};
   var pkgName = reporterConfig.suite || '';
-  var outputFile = helper.normalizeWinPath(path.resolve(config.basePath, reporterConfig.outputFile
-      || 'test-results.xml'));
+  var outputFile = helper.normalizeWinPath(
+    path.resolve(config.basePath, reporterConfig.outputFile || 'test-results.xml')
+  );
 
   var xml;
   var suites;
@@ -23,8 +28,10 @@ var JUnitReporter = function(baseReporterDecorator, config, logger, helper, form
     allMessages.push(msg);
   }];
 
-  var initliazeXmlForBrowser = function(browser) {
+  var initializeXmlForBrowser = function(browser) {
     var timestamp = (new Date()).toISOString().substr(0, 19);
+
+    // TODO don't set this
     var suite = suites[browser.id] = xml.ele('testsuite', {
       name: browser.name,
       'package': pkgName,
@@ -32,6 +39,7 @@ var JUnitReporter = function(baseReporterDecorator, config, logger, helper, form
       id: 0,
       hostname: os.hostname()
     });
+
     suite.ele('properties').ele('property', {name: 'browser.fullName', value: browser.fullName});
   };
 
@@ -40,16 +48,17 @@ var JUnitReporter = function(baseReporterDecorator, config, logger, helper, form
     xml = builder.create('testsuites');
 
     // TODO(vojta): remove once we don't care about Karma 0.10
-    browsers.forEach(initliazeXmlForBrowser);
+    browsers.forEach(initializeXmlForBrowser);
   };
 
   this.onBrowserStart = function(browser) {
-    initliazeXmlForBrowser(browser);
+    initializeXmlForBrowser(browser);
   };
 
   this.onBrowserComplete = function(browser) {
     var suite = suites[browser.id];
 
+    // TODO check if suites is empty instead
     if (!suite) {
       // This browser did not signal `onBrowserStart`. That happens
       // if the browser timed out during the start phase.
@@ -89,20 +98,52 @@ var JUnitReporter = function(baseReporterDecorator, config, logger, helper, form
     allMessages.length = 0;
   };
 
-  this.specSuccess = this.specSkipped = this.specFailure = function(browser, result) {
-    var spec = suites[browser.id].ele('testcase', {
-      name: result.description, time: ((result.time || 0) / 1000),
-      classname: (pkgName ? pkgName + ' ' : '') + result.suite.join('.').replace(/\./g, '_')
-    });
+  // Creates the testsuite element for this suite and its parents
+  this.createSuiteElements = function (suiteArr) {
+    var suiteName = suiteArr.join('.');
 
-    if (result.skipped) {
-      spec.ele('skipped');
-    }
+    // console.log(suiteName);
+    if (!suites[suiteName]) {
 
-    if (!result.success) {
-      result.log.forEach(function(err) {
-        spec.ele('failure', {type: ''}, formatError(err));
+      suiteArr.pop();
+
+      // don't recurse if no items left after popping
+      if (!suites[suiteArr.join('.')] && suiteArr.length > 0) {
+        this.createSuiteElements(suiteArr);
+      }
+
+      var timestamp = (new Date()).toISOString().substr(0, 19);
+
+      suites[suiteName] = xml.ele('testsuite', {
+        name: suiteName,
+        timestamp: timestamp,
+        id: 0,
       });
+    }
+  };
+
+  this.specSuccess = this.specSkipped = this.specFailure = function(browser, result) {
+    // be sure to pass a copy, not the original, since this will alter the array we give it
+    this.createSuiteElements(result.suite.slice());
+
+    var suiteName = result.suite.join('.');
+
+    if (suiteName !== '') {
+      var spec = suites[suiteName].ele('testcase', {
+        classname: (pkgName ? pkgName + ' ' : '') + result.suite.join('.'),
+        name: result.description,
+        time: ((result.time || 0) / 1000)
+      });
+
+      if (result.skipped) {
+        spec.ele('skipped');
+      }
+
+      if (!result.success) {
+        result.log.forEach(function(err) {
+          spec.ele('failure', {type: ''}, formatError(err));
+        });
+      }
     }
   };
 
